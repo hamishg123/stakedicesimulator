dd4a581a465b2c26e4cba5a2f28dc75930807ab6
hamishg123 <hamishgordon051@gmail.com>
2025-11-27 08:45:44 +0000

    rv

--- script.js
+++ script.js
@@ -3,4 +3,4 @@ const toptext = document.getElementById("headtext");
 const slider = document.getElementById("slider");
 let rv = Math.random(0,100);
 slider.value = rv
-toptext.textContent = "rv";
+toptext.textContent = rv;
