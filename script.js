7da0db24497d1e84944d233fc5765f44134b5ce3
hamishg123 <hamishgordon051@gmail.com>
2025-11-27 15:57:47 +0000

    bet function

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
 const toptext = document.getElementById("headtext");
 const slider = document.getElementById("slider");
+const balence = document.getElementById("balence");
+const stake = document.getElementById("stake");
+const over = document.getElementById("over");
+
 let rv = Math.floor(Math.random() * 101);
 slider.value = rv;
 toptext.textContent = rv;
+
+function bet() {
+    if (balence <= stake) {
+        toptext.textContent = "You Dont have Enough balence!";
+        toptext.style.color = "red"
+    }
+}
