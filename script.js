a6e32e3f01f27f4755e9735d3fc868e11805ca51
hamishg123 <hamishgordon051@gmail.com>
2025-11-27 08:53:48 +0000

    *100

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 const toptext = document.getElementById("headtext");
 const slider = document.getElementById("slider");
-let rv = Math.random(0,100);
+let rv = Math.random(0,100) * 100;
 slider.value = rv
 toptext.textContent = rv;
