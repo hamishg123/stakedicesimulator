3dbe4e4e895e6c5c8fac250e3a7857f4f013f98a
hamishg123 <hamishgordon051@gmail.com>
2025-11-27 08:22:00 +0000

    margin

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
 const toptext = document.getElementById("headtext");
 const slider = document.getElementById("slider");
-slider.style.paddingLeft = Math.random(1,100) + "%"
-toptext.textContent = "test";
+slider.style.marginInlineStart = Math.random(0,100) + "%"
+toptext.textContent = "test1";
